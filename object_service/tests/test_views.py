a47974e3874cd21afab3a53e6d4c015c527b0239
Edwin Henneken <ehenneken@gmail.com>
2018-09-14 14:06:47 -0400

    improved coverage unittests

--- object_service/tests/test_views.py
+++ object_service/tests/test_views.py
@@ -19,12 +19,53 @@ class TestViews(TestCase):
         app_ = app.create_app()
         return app_
 
+    @httpretty.activate
     def test_empty_post_request(self):
         '''test ObjectSearch view - empty request'''
         from object_service.views import ObjectSearch
-
+        QUERY_URL = self.app.config.get('OBJECTS_SIMBAD_TAP_URL')
+        mockdata =  {"data":[[1575544, "NAME ANDROMEDA","NAME ANDROMEDA"],[3133169, "NAME LMC", "NAME LMC"]]}
+        def request_callback(request, uri, headers):
+            data = request.body
+            return (200, headers, '%s'%json.dumps(mockdata))
+        # Mock the reponse
+        httpretty.register_uri(
+            httpretty.POST, QUERY_URL,
+            content_type='application/json',
+            status=200,
+            body=request_callback)
         ObjSrch = ObjectSearch()
         # First do empty post request
         result = ObjSrch.post()
         expected = ({'Error Info': 'No identifiers/objects found in POST body', 'Error': 'Unable to get results!'}, 200)
         self.assertEqual(result, expected)
+
+    @httpretty.activate
+    def test_post_request_exception(self):
+        '''test ObjectSearch view - empty request'''
+        from object_service.views import ObjectSearch
+        QUERY_URL = self.app.config.get('OBJECTS_SIMBAD_TAP_URL')
+        QUERY_URL_CDS = self.app.config.get('OBJECTS_SIMBAD_TAP_URL_CDS')
+        mockdata =  {"data":[[1575544, "NAME ANDROMEDA","NAME ANDROMEDA"],[3133169, "NAME LMC", "NAME LMC"]]}
+        def request_callback(request, uri, headers):
+            data = request.body
+            status = 200
+            if data.find('TOP') == -1:
+                return (200, headers, '%s'%json.dumps(mockdata))
+            else:
+                raise Exception('Problem with CfA TAP service!')
+        # Mock the reponse
+        httpretty.register_uri(
+            httpretty.POST, QUERY_URL,
+            content_type='application/json',
+            status=200,
+            body=request_callback)
+        httpretty.register_uri(
+            httpretty.POST, QUERY_URL_CDS,
+            content_type='application/json',
+            status=200,
+            body=request_callback)
+        ObjSrch = ObjectSearch()
+        # First do empty post request
+        result = ObjSrch.post()
+        self.assertEqual(self.app.config.get('OBJECTS_SIMBAD_TAP_URL'), self.app.config.get('OBJECTS_SIMBAD_TAP_URL_CDS'))
